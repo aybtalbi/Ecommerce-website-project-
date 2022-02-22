9a76f9ec71e5f0c6b1d6099a9b143916df83e9c4
aybtalbi <aybtalbi99@gmail.com>
2022-02-22 11:07:00 +0100

    commit 1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Announcement from "../components/Announcement";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
 
 const home = () => {
-  return (
-      <div>
-      <Announcement/>
-      <NavBar/>
-      <Slider/>
-      </div>
+    return (
+        <div>
+            <Announcement/>
+            <NavBar/>
+            <Slider/>
+            <Categories/>
+            <Products/>
+        </div>
 
-  )
+    )
 }
 
 export default home
